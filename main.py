9e8ec847a2a4ebf114e1b40a60470550f7754d31
Shivani Chauhan <shivanichauhandev.@gmail.com>
2024-03-12 11:23:51 +0530

    modified the main file

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import PyPDF2
 import pyttsx3
 
-user = input("please enter the pdf path: ")
-path = open(user,"rb")
+User = input("please enter the pdf path: ")
+Page_no =int(input("please enter the page number: "))
+path = open(User,"rb")
 
 reader = PyPDF2.PdfReader(path) 
 
 
-from_page = reader.pages[1]
+from_page = reader.pages[Page_no]
 
 text = from_page.extract_text()
 
